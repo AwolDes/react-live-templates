f18ce73f3f1121a4b1fa5a0792c7b223286a7866
Austin Wilshire <awoldes@gmail.com>
2017-01-16 09:37:34 +1000

    Create crc.js

---
+++ crc.js
@@ -0,0 +1,29 @@
+// React Component
+import React, { Component, PropTypes } from "react";
+
+class $COMPONENT$ extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+    
+    };
+  }
+  
+  componentDidMount() {
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Component!</h1>
+      </div>
+    );
+  };
+}
+
+$COMPONENT$.propTypes = {
+
+}
+
+export default $COMPONENT$;
