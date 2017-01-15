d123b6bcbac9f0c314bd682f9a2c2d7bc244aae5
Austin Wilshire <awoldes@gmail.com>
2017-01-16 09:35:34 +1000

    Create crsc.js

---
+++ crsc.js
@@ -0,0 +1,22 @@
+// Stateless React Component
+import React, { PropTypes } from "react";
+
+const $COMPONENT$ = (props) => {
+
+  const BlankFunc = (param) => {
+  };
+
+  return (
+    <div>
+      <h1>Stateless component!!</h1>
+    </div>
+  );
+};
+
+$COMPONENT$.propTypes = {
+};
+
+$COMPONENT$.defaultProps = {
+};
+
+export default $COMPONENT$;
